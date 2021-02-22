f81e866b18d60b770b229afe7ad8e97e42356486
aqrit <aqrit@aqr.local>
2021-02-23 08:49:32 +1100

    added dependencies and server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller.js");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost: " + PORT);
+}); 
