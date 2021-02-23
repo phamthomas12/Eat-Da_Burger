4ec411a83329e52ca246340edd0ce5814efd0751
phamthomas12 <phamthomas12@gmail.com>
2021-02-23 19:41:55 +1100

    fixing minor issues

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
+const path = require("path");
 
 const app = express();
 
